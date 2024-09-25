f2362a65d492b59e9410687016f47269a98e083d
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-25 21:54:49 +0530

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -10,12 +10,21 @@ users_collection = db["users"] if db is not None else None  # Users collection
 
 @Client.on_message(~filters.service, group=1)
 async def track_users(_, msg: Message):
-    if users_collection is None:
-        return
+    if users_collection is None or msg.from_user is None:
+        return  # Ignore messages without a valid user
 
     user_id = msg.from_user.id
-    if not users_collection.find_one({"_id": user_id}):
-        users_collection.insert_one({"_id": user_id, "first_name": msg.from_user.first_name})
+    first_name = msg.from_user.first_name
+    username = msg.from_user.username
+
+    if not users_collection.find_one({"user_id": user_id}):
+        users_collection.insert_one({
+            "user_id": user_id,
+            "first_name": first_name,
+            "username": username,
+            "date_added": datetime.utcnow()
+        })
+
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def get_stats(_, msg: Message):
