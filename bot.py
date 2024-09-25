1e9d7649b2044edda2b0eb7c0fb1a6e466706718
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-25 21:41:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 import env
-import logging
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 
-logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
 app = Client(
     "Session_bot",
@@ -18,7 +16,6 @@ app = Client(
 
 
 if __name__ == "__main__":
-    logging.info("Starting the bot")
     try:
         app.start()
     except (ApiIdInvalid, ApiIdPublishedFlood):
@@ -26,7 +23,7 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.me.username
-    logging.info(f"@{uname} is now running!")
+    print(f"@{uname} is now running!")
     idle()
     app.stop()
-    logging.info("Bot stopped. Alvida!")
+    print("Bot stopped. Alvida!")
