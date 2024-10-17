e8345cf5f525962efab28c0a0e0e624b4241cf8f
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-10-17 16:09:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 from flask import Flask
-flask_app = Flask(__name__)
+from threading import Thread
 
 logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
@@ -17,7 +17,7 @@ app = Client(
     plugins={'root':'StringSessionBot'},
 )
 
-
+flask_app = Flask(__name__)
 @flask_app.route('/')
 def hello_world():
     return 'Bot Alive'
